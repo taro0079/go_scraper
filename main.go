e903ffc953c191b3e5c3cf958e6ddc6fafb7afbb
Taro Morita <morita0079@gmail.com>
2022-01-04 21:19:19 +0900

    formatter

--- main.go
+++ main.go
@@ -7,14 +7,11 @@ import (
 	"github.com/taro0079/scraping_homes/scraper"
 )
 
-
-
 func main() {
 	url := "https://www.homes.co.jp/chintai/room/a3ee0fd00c2434192dea235ae73d489a2d568608/?bid=37031820006952"
 	s := scraper.NewScraper(url)
 	mapdata := s.GetDetailTable()
 	formatted := scraper.NewTextFormatting(mapdata)
 	fmt.Println(formatted.RentFee())
-	
 
 }
